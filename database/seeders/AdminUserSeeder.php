a364fb52424ac3a177bf16c18fa43d9c4f446b26
Sam  Hopkinson <sam.hopkinson@purrgroup.com>
2021-08-27 19:58:23 +0100

    Added inertia, seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => env('ADMIN_USER_EMAIL'),
+            'password' => Hash::make(env('ADMIN_USER_PW'))
+        ]);
+    }
+}
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <Head title="Peter the Plantsman" />
+    <p>{{ laravelVersion }}</p>
+</template>
+
+
+<script>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+export default {
+    components: {
+      Head,
+      Link,
+    },
+    props: {
+        canLogin: Boolean,
+        canRegister: Boolean,
+        laravelVersion: String,
+        phpVersion: String,
+    },
+}
+</script>
